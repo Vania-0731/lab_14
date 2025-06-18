9a79ee67b4ab6f19a8735785503db0809fdcadc8
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-18 14:23:23 -0500

    Created LoginSerializer and UserSerializer

--- backend/series/serializer.py
+++ backend/series/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Serie, Category
-
+from django.contrib.auth.models import User
 
 class SerieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,12 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'description')
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
